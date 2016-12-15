2f11e50fc8ca6118c72902c82aa06dc4c876dcd1
Kota Weaver <kweaver@bostonic.net>
2016-12-15 13:20:10 -0500

    add initial examples

---
+++ src/template_multiple_variadics.cpp
@@ -0,0 +1,35 @@
+/*
+ * Demonstration of using multiple variadic templates
+ */
+
+#include <tuple>
+
+template <typename ...Ts>
+struct Pack { };
+
+template <typename ...Ts>
+class Foo;
+
+template <typename ...Ts, typename ...Us>
+struct Foo<Pack<Ts...>, Pack<Us...> > {
+    std::tuple<Ts...> first;
+    std::tuple<Us...> second;
+};
+
+#include <iostream>
+#include <string>
+
+struct Bar {
+    std::string str;
+    Bar() { }
+    Bar(std::string str) : str(str) { }
+};
+
+int main() {
+    Foo<Pack<int, std::string, float>, Pack<Bar, char> > foo;
+
+    std::get<std::string>(foo.first) = "hello";
+    std::get<Bar>(foo.second) = Bar("bar");
+
+    std::cout << std::get<std::string>(foo.first) << " " << std::get<Bar>(foo.second).str << std::endl;
+}
---
+++ src/template_tuples.cpp
@@ -0,0 +1,41 @@
+/*
+ * This example shows how to use a tuple to use arbitrary types in a field
+ * for a class.
+ */ 
+
+#include <tuple>
+
+template <typename ...Ts>
+class Foo {
+    std::tuple<Ts...> m_tup;
+
+public:
+    template <typename T>
+    void set(T val) { std::get<T>(m_tup) = val; }
+
+    template <typename T>
+    T get() { return std::get<T>(m_tup); }
+};
+
+#include <iostream>
+#include <string>
+
+class Bar {
+    std::string m_str;
+
+public:
+    Bar() { }
+    Bar(std::string str) : m_str(str) { }
+    void operator()() { std::cout << "hi my name is " << m_str << std::endl; }
+};
+
+int main() {
+    Foo<int, std::string, Bar> foo;
+
+    foo.set<Bar>(Bar("kota"));
+    foo.set<int>(4);
+    foo.set<std::string>("baz");
+
+    foo.get<Bar>()();
+    std::cout << foo.get<int>() << " " << foo.get<std::string>() << std::endl;
+}
